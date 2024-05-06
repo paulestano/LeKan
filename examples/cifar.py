fd3bb82397f42575535885b08d1c6d3418e716dc
Paul Estano <paul.estano@gmail.com>
2024-05-06 14:41:34 +0200

    convolutional kan

--- examples/cifar.py
+++ examples/cifar.py
@@ -60,7 +60,7 @@ import torchvision
 import torchvision.transforms as transforms
 from tqdm import tqdm
 
-from efficient_kan.kan import KANConv2d
+from efficient_kan.kan import KANConv2d, KANLinear
 
 ########################################################################
 # The output of torchvision datasets are PILImage images of range [0, 1].
@@ -128,22 +128,22 @@ def main():
     import torch.nn.functional as F
 
 
-    class Net(nn.Module):
+    class LeKan(nn.Module):
         def __init__(self):
             super().__init__()
             self.conv1 = KANConv2d(3, 6, 5)
             self.pool = nn.MaxPool2d(2, 2)
             self.conv2 = KANConv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
+            self.fc1 = KANLinear(16 * 5 * 5, 120)
+            self.fc2 = KANLinear(120, 84)
+            self.fc3 = KANLinear(84, 10)
 
         def forward(self, x):
             x = self.pool(self.conv1(x))
             x = self.pool(self.conv2(x))
             x = torch.flatten(x, 1) # flatten all dimensions except batch
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
+            x = self.fc1(x)
+            x = self.fc2(x)
             x = self.fc3(x)
             return x
 
