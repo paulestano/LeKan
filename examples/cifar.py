58b1ee149601281a9b47a4dd7afd95298c27060b
Paul Estano <paul.estano@gmail.com>
2024-05-06 19:21:41 +0200

    add order for 2nd conv

--- examples/cifar.py
+++ examples/cifar.py
@@ -133,7 +133,7 @@ def main():
             super().__init__()
             self.conv1 = KANConv2d(3, 6, 5, spline_order=5)
             self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = KANConv2d(6, 16, 5)
+            self.conv2 = KANConv2d(6, 16, 5, spline_order=5)
             self.fc1 = KANLinear(16 * 5 * 5, 120)
             self.fc2 = KANLinear(120, 84)
             self.fc3 = KANLinear(84, 10)
